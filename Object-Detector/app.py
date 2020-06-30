d83037efdc65115eba64f21ac4c4ae8d5d0786ca
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2020-06-30 12:41:16 +0530

    Add files via upload

---
+++ Object-Detector/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, Response,  request, session, redirect, url_for, send_from_directory, flash
+from werkzeug.utils import secure_filename
+
+from PIL import Image
+import os
+import sys
+import cv2
+from app_helper import *
+
+# __author__ = ''
+# __source__ = ''
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'C:/Flask/DetectWeb/static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        # create a secure filename
+        filename = secure_filename(f.filename)
+        print(filename)
+        # save file to /static/uploads
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print(filepath)
+        f.save(filepath)
+        get_image(filepath, filename)
+
+        return render_template("uploaded.html", display_detection=filename, fname=filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
